366930b593311980b8ec7602a36e2ecea86499a3
K_G <kevin.gonzalez115@gmail.com>
2020-02-02 00:54:00 -0800

    Decided to go with a csv instead. It made things so much more efficiant, organized, and more readable!

---
+++ test.py
@@ -0,0 +1,40 @@
+import bs4 as bs
+import datetime as dt
+import os
+import pandas as pd
+import pandas_datareader.data as web
+import requests
+import lxml
+import csv
+
+def top100_crypto():
+    resp = requests.get('https://coinmarketcap.com/all/views/all/')
+    soup = bs.BeautifulSoup(resp.text, "lxml")
+    # "Table" in coinmarketcap is not named, So i find the div before it.
+    table = soup.find('div', {'class': 'cmc-table__table-wrapper-outer'})
+    # then I tell bs4 to find the tbody after that.
+    table_sorted = table.find_next('tbody')
+    names = []
+    symbols = []
+    prices = []
+    volumes = []
+
+    for row in table_sorted.findAll('tr')[:100]:
+        name = row.findAll('td')[1].text
+        names.append(name)
+        symbol = row.findAll('td')[2].text
+        symbols.append(symbol)
+        price = row.findAll('td')[4].text
+        prices.append(price)
+        volume = row.findAll('td')[6].text
+        volumes.append(volume)
+
+    with open("crypto_data.csv", 'w', newline='') as f:
+        columns = ['Name', 'Symbol', 'Price', 'Volume']
+        the_writer = csv.DictWriter(f, fieldnames=columns)
+        the_writer.writeheader()
+        n = 0
+        for i in range(100):
+            the_writer.writerow({'Name': names[n], 'Symbol': symbols[n], 'Price': prices[n], 'Volume': volumes[n] })
+            n += 1
+top100_crypto()
